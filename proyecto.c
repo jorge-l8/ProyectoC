75880f39661f9ef72ed0eb1612a03c9c7b0eb063
jorge-l8 <73497913+jorge-l8@users.noreply.github.com>
2020-11-01 17:34:59 -0400

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 int main(void){
+<<<<<<< Updated upstream
 	
+=======
+	int op;
+	/*Interfaz*/
+	do{
+		/*Imprimir opciones*/
+		switch(op){
+			case 1:
+				/*Codigo*/
+			case 2:
+				/*Codigo*/
+			case 3:
+				/*Codigo*/
+			case 4:
+				/*Codigo*/
+			default:
+				/*Codigo*/
+		}
+	}while(op!=0);
+>>>>>>> Stashed changes
 	return 0;
 }
