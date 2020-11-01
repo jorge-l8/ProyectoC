f3152017fe544ddf0c692d528a3db1ff9a786389
jorge-l8 <73497913+jorge-l8@users.noreply.github.com>
2020-11-01 17:09:05 -0400

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 int main(void){
+	int op;
+	/*Interfaz*/
+	do{
+		switch(op){
+			case 1:
+				/*Codigo*/
+			case 2:
+				/*Codigo*/
+			case 3:
+				/*Codigo*/
+			case 4:
+				/*Codigo*/
+			default:
+				/*Codigo*/
+		}
+	}while(op!=0);
 	return 0;
 }
