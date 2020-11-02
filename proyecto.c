af1994f86da8aaf438a4fb78e54ae54e4a8849c8
jorge-l8 <73497913+jorge-l8@users.noreply.github.com>
2020-11-02 09:45:16 -0400

    Revert "Update proyecto.c"
    This reverts commit a49f080c642db99ffb3781696ea6c3952a812a52.

--- proyecto.c
+++ proyecto.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
 int main(void){
 	int op;
 	/*Interfaz*/
 	do{
-		/*Imprimir opciones*****/
 		switch(op){
 			case 1:
 				/*Codigo*/
 			case 2:
 				/*Codigo*/
-				printf("1.-Agregar empresa");
-				printf("2.-Modificar empresa");
-				printf("3.-Consultar empresa");
-				printf("4.-Eliminar empresa");
-				printf("0.-Volver a menu inicial");
+			case 3:
 				/*Codigo*/
 			case 4:
 				/*Codigo*/
