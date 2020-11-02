3472e74e2e52b84111c4609a01b992ade239c408
jorge-l8 <73497913+jorge-l8@users.noreply.github.com>
2020-11-02 09:45:17 -0400

    Revert "Update proyecto.c"
    This reverts commit f3152017fe544ddf0c692d528a3db1ff9a786389.

--- proyecto.c
+++ proyecto.c
@@ -1,20 +1,4 @@
 #include <stdio.h>
 int main(void){
-	int op;
-	/*Interfaz*/
-	do{
-		switch(op){
-			case 1:
-				/*Codigo*/
-			case 2:
-				/*Codigo*/
-			case 3:
-				/*Codigo*/
-			case 4:
-				/*Codigo*/
-			default:
-				/*Codigo*/
-		}
-	}while(op!=0);
 	return 0;
 }
