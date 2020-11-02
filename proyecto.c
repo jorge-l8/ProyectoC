fff321767afdba270f25704c0a3fc6a725d32ba8
Piero0108 <73797915+Piero0108@users.noreply.github.com>
2020-11-02 11:30:52 -0400

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -1,7 +1,24 @@
+/*
+Primera entrega Proyecto
+Integrantes
+Jose Felipe: 28.312.749
+Jorge Leon: 27.580.573
+Pedro Leal: 30.032.715
+Gianpiero Yorlano: 27.671.343
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(void){
+	char nombre1[]="Jorge Leon",nombre2[]="Jose Felipe",nombre3[]="Pedro Leal",nombre4[]="Gianpiero Yorlano";
+	printf("\t\t Sistema %cummy  ",165);
+	printf("\n\n\tIntegrantes del Proyecto: ");
+	printf("\n\n\t%s \n\t%s \n\t%s \n\t%s\n\n",nombre1,nombre2,nombre3,nombre4);
+	system("PAUSE");
+	system("CLS");
+
 <<<<<<< Updated upstream
-	
 =======
 	int op;
 	/*Interfaz*/
