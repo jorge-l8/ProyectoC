1c138c524f02ae08a2498b2b17e2dec4d3a5627c
Piero0108 <73797915+Piero0108@users.noreply.github.com>
2020-11-03 16:02:51 -0400

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -1,3 +1,11 @@
+/*
+Primera entrega Proyecto
+Integrantes
+Jose Felipe: 28.312.749
+Jorge Leon: 27.580.573
+Pedro Leal: 30.032.715
+Gianpiero Yorlano: 27.671.343
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -351,10 +359,8 @@ int main(void)
  	
 void mostrarportada(void)
 {	 
-	char hola[]="Sistema Nummy",nombre1[]="Jorge Leon",nombre2[]="Jose Felipe",nombre3[]="Pedro Leal",nombre4[]="Gianpiero Yorlano";
-	printf("\t\t%s",hola);
 	char nombre1[]="Jorge Leon",nombre2[]="Jose Felipe",nombre3[]="Pedro Leal",nombre4[]="Gianpiero Yorlano";
-	printf("\t\tSistema");printf(" %c",165); printf("ummy");
+	printf("\t\t Sistema %cummy  ",165);
 	printf("\n\n\tIntegrantes del Proyecto: ");
 	printf("\n\n\t%s \n\t%s \n\t%s \n\t%s\n\n",nombre1,nombre2,nombre3,nombre4);
 	system("PAUSE");
